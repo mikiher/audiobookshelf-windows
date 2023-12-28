dbad74b805bea45760b5b69572c80fb2268d10d3
mikiher <mikiher@gmail.com>
2023-12-28 17:20:48 +0200

    Add single instance chck

--- Program.cs
+++ Program.cs
@@ -1,16 +1,27 @@
 using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace AudiobookshelfTray
 {
-    internal static class Program
+   internal static class Program
     {
+        static readonly Mutex _mutex = new(true, "AudiobookshelfTray");
+ 
         [STAThread]
         static void Main()
         {
             // SetProcessDPIAware() is required to prevent text controls from being blurry on high DPI screens.
             SetProcessDPIAware();
-            Application.Run(new AppTray());
+            if (_mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.Run(new AppTray());
+                _mutex.ReleaseMutex();
+            }
+            else
+            { 
+                MessageBox.Show("AudiobookshelfTray is already running.\nCheck the system tray", "Audiobookshelf", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         private static extern bool SetProcessDPIAware();
